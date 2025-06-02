5892c1edf46b8c35b45bc1c94ee298f1aa502b28
Zenthro <165419716+zenthro-dev@users.noreply.github.com>
2025-06-02 19:17:43 +0000

    Grant admin privileges

---
+++ supabase/migrations/20250602191738_fading_hill.sql
@@ -0,0 +1,8 @@
+/*
+  # Grant admin privileges to user
+  
+  Adds the user to the admin_users table with admin role
+*/
+
+INSERT INTO admin_users (user_id, role)
+VALUES ('YOUR_USER_ID', 'admin');
